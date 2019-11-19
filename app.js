9a98286e75d7b6a68b071764f4871b90be6f1635
Alex Furrow <alex.p.furrow@gmail.com>
2019-11-19 13:40:18 -0500

    Refactoring.

--- app.js
+++ app.js
@@ -1,73 +1,82 @@
+const argv = require("yargs").argv;
 const fs = require("fs");
 const web = require("./web.js");
 const categoryMaps = require("./categoryMaps.json");
 
-async function GetCategories() {
-    const browser = await web.browser();
-    const page = await browser.newPage();
-    await page.goto("https://ark.gamepedia.com/Item_IDs");
+async function main() {
+    const browser = await web.getBrowser();
 
     fs.mkdirSync("dist/images", { recursive: true });
 
-    let categories = [];
+    if (argv.i) {
+        const items = await getItems();
 
-    for (var i = 0; i < categoryMaps.length; i++) {
-        let category = { name: categoryMaps[i].name, items: [] };
-
-        await page.click(`div[data-page="${categoryMaps[i].dataPage}"] h3 span.jslink`);
-        await page.waitForSelector(`div.pageloader-contentloaded[data-page="${categoryMaps[i].dataPage}"]`);
-
-        let results = await page.evaluate(dataPage => {
-            let rows = document.querySelectorAll(`div[data-page="${dataPage}"] tbody tr`);
-            let items = [];
-
-            for (const r of rows) {
-                // check the class name and blueprint cells
-                // if there is no class name or blueprint data ignore it
-                if (r.children[4].children.length > 0 && r.children[5].children.length > 0) {
-                    let id = Number.parseInt(r.children[3].innerText);
-
-                    items.push({
-                        image: r.children[0].querySelector("a.image").getAttribute("href"),
-                        stackSize: Number.parseInt(r.children[2].innerText),
-                        id: Number.isNaN(id) ? null : id,
-                        className: r.children[4].querySelector("span").innerText,
-                        blueprintPath: r.children[5].querySelector("span").innerText
-                    });
-                }
+        for (let i = 0; i < items.length; i++) {
+            try {
+                const fn = await web.getImage(items[i].imagePage, "dist/images");
+                items[i].imagePath = fn;
+            } catch (error) {
+                console.log(error);
             }
-
-            return items;
-        }, categoryMaps[i].dataPage);
-
-        const imgPage = await browser.newPage();
-
-        for (var x = 0; x < results.length; x++) {
-            let imgSelector = decodeURI(`img[alt="${results[x].image.substr(1).replace(/_/g, " ")}"]`);
-            console.log(imgSelector);
-            await imgPage.goto("https://ark.gamepedia.com" + results[x].image);
-            await imgPage.waitForSelector(imgSelector);
-
-            let imgUrl = await imgPage.evaluate(imgSelector => {
-                return document.querySelector(imgSelector).getAttribute("src");
-            }, imgSelector);
-
-            web.getImage(imgUrl, "dist/images");
         }
 
-        imgPage.close();
-
-        category.items = results.slice();
-        categories.push(category);
+        fs.writeFile("dist/items.json", JSON.stringify(items, null, 4), error => {
+            if (error) {
+                console.log(error);
+            }
+        });
     }
-
-    fs.writeFile("dist/items.json", JSON.stringify(categories, null, 4), error => {
-        if (error) {
-            console.log(error);
-        }
-    });
 
     browser.close();
 }
 
-GetCategories();
+async function getItems() {
+    const browser = await web.getBrowser();
+    const page = await browser.newPage();
+    await page.goto(`${web.baseUrl}/Item_IDs`);
+
+    let items = [];
+
+    for (var i = 0; i < categoryMaps.length; i++) {
+        await page.click(`div[data-page="${categoryMaps[i].dataPage}"] h3 span.jslink`);
+        await page.waitForSelector(`div.pageloader-contentloaded[data-page="${categoryMaps[i].dataPage}"]`);
+
+        let results = await page.evaluate(
+            (dataPage, baseUrl, category) => {
+                let rows = document.querySelectorAll(`div[data-page="${dataPage}"] tbody tr`);
+                let catItems = [];
+
+                for (const r of rows) {
+                    // check the class name and blueprint cells
+                    // if there is no class name or blueprint data ignore it
+                    if (r.children[4].children.length > 0 && r.children[5].children.length > 0) {
+                        let id = Number.parseInt(r.children[3].innerText);
+
+                        catItems.push({
+                            id: Number.isNaN(id) ? null : id,
+                            name: baseUrl + r.children[0].querySelector("a[title]").innerText,
+                            category: category,
+                            imagePage: baseUrl + r.children[0].querySelector("a.image").getAttribute("href"),
+                            imagePath: null,
+                            wikiUrl: baseUrl + r.children[0].querySelector("a[title]").getAttribute("href"),
+                            stackSize: Number.parseInt(r.children[2].innerText),
+                            className: r.children[4].querySelector("span").innerText,
+                            blueprintPath: r.children[5].querySelector("span").innerText
+                        });
+                    }
+                }
+
+                return catItems;
+            },
+            categoryMaps[i].dataPage,
+            web.baseUrl,
+            categoryMaps[i].name
+        );
+
+        items = items.concat(results);
+    }
+
+    return items;
+}
+
+main();
--- web.js
+++ web.js
@@ -2,20 +2,45 @@ const argv = require("yargs").argv;
 const puppeteer = require("puppeteer");
 const download = require("image-downloader");
 
-let instance;
-let semaphore = false;
-const getInstance = async () => {
-    if (!instance && !semaphore) {
-        semaphore = true; // mark awaited constructor
-        instance = await puppeteer.launch({ headless: argv.d ? true : false, userDataDir: "./cache" });
+module.exports.baseUrl = "https://ark.gamepedia.com";
+
+let browserInstance;
+let broswerSemaphore = false;
+module.exports.getBrowser = async () => {
+    if (!browserInstance && !broswerSemaphore) {
+        broswerSemaphore = true; // mark awaited constructor
+        browserInstance = await puppeteer.launch({ headless: argv.d ? false : true, userDataDir: "./cache" });
     }
-    return instance;
+    return browserInstance;
 };
 
-async function getImage(url, file) {
-    const result = await download.image({ url: url, dest: file });
-    console.log(`downloaded ${url} to ${result.filename}`);
+let imgPageInstance;
+let imgPageSemaphore = false;
+async function getImgPage() {
+    if (!imgPageInstance && !imgPageSemaphore) {
+        imgPageSemaphore = true; // mark awaited constructor
+        const b = await module.exports.getBrowser();
+        imgPageInstance = await b.newPage();
+    }
+    return imgPageInstance;
 }
 
-module.exports.browser = getInstance;
-module.exports.getImage = getImage;
+module.exports.getImage = async (url, dest) => {
+    const imgSelector = decodeURI(
+        `img[alt="${url
+            .replace(module.exports.baseUrl, "")
+            .substr(1)
+            .replace(/_/g, " ")}"]`
+    );
+    const ip = await getImgPage();
+    await ip.goto(url);
+    await ip.waitForSelector(imgSelector);
+
+    const imgUrl = await ip.evaluate(imgSelector => {
+        return document.querySelector(imgSelector).getAttribute("src");
+    }, imgSelector);
+
+    const result = await download.image({ url: imgUrl, dest: dest });
+    console.log(`downloaded ${url} to ${result.filename}`);
+    return result.filename;
+};
