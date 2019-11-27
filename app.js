18c4b4f03320fbe82fbf9b053487463dd20e0a14
Alex Furrow <alex.p.furrow@gmail.com>
2019-11-27 14:52:21 -0500

    Implemented creature scraping.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const argv = require("yargs").argv;
 const fs = require("fs");
 const path = require("path");
+const logger = require("tracer").colorConsole({ level: argv.d ? "debug" : "warn" });
 const web = require("./web.js");
 const itemCategoryMaps = require("./itemCategoryMaps.json");
-const logger = require("tracer").colorConsole({ level: argv.d ? "debug" : "warn" });
+const creatureDlcMaps = require("./creatureDlcMaps.json");
 
 async function main() {
     const browser = await web.getBrowser();
@@ -32,7 +33,69 @@ async function main() {
         logger.info(`${items.length} items scraped.`);
     }
 
+    if (argv.c) {
+        logger.info("Scraping ark creatures...");
+        const creatures = await getCreatures();
+
+        for (let i = 0; i < creatures.length; i++) {
+            try {
+                const fn = await web.getImage(creatures[i].imagePage, "dist/images");
+                creatures[i].imageName = path.parse(fn).base;
+            } catch (error) {
+                logger.error(error);
+            }
+        }
+
+        fs.writeFile("dist/creatures.json", JSON.stringify(creatures, null, 4), error => {
+            if (error) {
+                logger.error(error);
+            }
+        });
+
+        logger.info(`${creatures.length} creatures scraped.`);
+    }
+
     browser.close();
+}
+
+async function getCreatures() {
+    const containerSelector = `div[id="mw-content-text"] div.mw-parser-output`;
+    const browser = await web.getBrowser();
+    const page = await browser.newPage();
+
+    await page.goto(`${web.baseUrl}/Creature_IDs`);
+    await page.waitForSelector(containerSelector);
+
+    let results = await page.evaluate(
+        (containerSelector, baseUrl, creatureDlcMaps) => {
+            let tables = document.querySelectorAll(`${containerSelector} table`);
+            let items = [];
+
+            for (var i = 0; i < creatureDlcMaps.length; i++) {
+                let rows = tables[creatureDlcMaps[i].tableIndex].querySelectorAll(`tbody tr`);
+
+                for (const r of rows) {
+                    items.push({
+                        name: r.children[0].querySelector("a[title]").innerText,
+                        dlc: creatureDlcMaps[i].name,
+                        category: r.children[1].querySelector("a").innerText,
+                        imagePage: baseUrl + r.children[0].querySelector("a.image").getAttribute("href"),
+                        wikiUrl: baseUrl + r.children[0].querySelector("a[title]").getAttribute("href"),
+                        nameTag: r.children[2].innerText,
+                        entityId: r.children[3].innerText,
+                        blueprintPath: r.children[4].querySelector("span").innerText
+                    });
+                }
+            }
+
+            return items;
+        },
+        containerSelector,
+        web.baseUrl,
+        creatureDlcMaps
+    );
+
+    return results;
 }
 
 async function getItems() {
